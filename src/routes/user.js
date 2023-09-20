45ff178f0767da5cc5c63e140f153b2e409a891e
KwabenaAnning <134407997+KwabenaAnning@users.noreply.github.com>
2023-09-20 22:38:17 +0000

    backend-assignment

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router()
+const Express = require('express');
+const router = Express.Router()
 const { createUser, signInUser } = require('../controllers/user.controller')
 
 router.post('/signup', createUser);
